c2fccaf32e00f7d30039a15c7b571ab89b789233
vikesh rana <vikeshrana9@gmail.com>
2025-03-18 00:27:18 +0530

    handle test case - single number

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -3,3 +3,7 @@ import { stringCalculator } from "./utils";
 test("returns 0 for empty string", () => {
   expect(stringCalculator("")).toBe(0);
 });
+
+test("returns sum for single number", () => {
+  expect(stringCalculator("1")).toBe(1);
+});
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
 export function stringCalculator(numbers) {
     if (!numbers) return 0;
+    if (!numbers.includes(",") && !numbers.includes("\n")) return parseInt(numbers, 10);
   }
