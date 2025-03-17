96695042dcb885b8c4a90e1938f3cdc8e9a17dfb
vikesh rana <vikeshrana9@gmail.com>
2025-03-18 00:33:00 +0530

    handle test case - multiple numbers

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -7,3 +7,7 @@ test("returns 0 for empty string", () => {
 test("returns sum for single number", () => {
   expect(stringCalculator("1")).toBe(1);
 });
+
+test("returns sum for multiple numbers", () => {
+  expect(stringCalculator("1,2,3")).toBe(6);
+});
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,8 @@
 export function stringCalculator(numbers) {
     if (!numbers) return 0;
+    
     if (!numbers.includes(",") && !numbers.includes("\n")) return parseInt(numbers, 10);
+
+    let numArray = numbers.split(',').map(num => parseInt(num, 10));
+    return numArray.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
   }
